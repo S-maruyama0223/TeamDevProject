515f700f5137aff0d737bff5303e1fefdddcc769
user <aollzones@gmail.com>
2024-11-22 13:41:50 +0900

    フォーマットの設定が正しく行われていなかったのが原因でした。フォーマットの修正を行いました。

--- 2DActionGame/Assets/Scripts/Bullet.cs
+++ 2DActionGame/Assets/Scripts/Bullet.cs
@@ -2,21 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
-{
+public class Bullet : MonoBehaviour {
     //’e‘¬
     private float speed = 10f;
     //•ûŒü
     private Vector2 direction;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         direction = transform.right;
     }
 
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
         transform.Translate(direction * speed * Time.deltaTime);
     }
 }
