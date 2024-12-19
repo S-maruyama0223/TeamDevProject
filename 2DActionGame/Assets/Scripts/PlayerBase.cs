a73a217a8cd36f041d6adef544ff3f5246fad9c1
user <aollzones@gmail.com>
2024-12-19 23:40:35 +0900

    PlayerBase.csを編集し、画面外にプレイヤーが出ないように修正しました

--- 2DActionGame/Assets/Scripts/PlayerBase.cs
+++ 2DActionGame/Assets/Scripts/PlayerBase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerBase : MonoBehaviour{
+public class PlayerBase : MonoBehaviour {
     [SerializeField] protected FloatingJoystick floatingJoystick;
     [SerializeField] private float movementSpeed = 10f;
     virtual protected void Update() {
@@ -12,5 +12,14 @@ public class PlayerBase : MonoBehaviour{
         movementDirection = floatingJoystick.Direction;
 
         transform.Translate(movementDirection * movementSpeed * Time.deltaTime);
+
+        //画面の左下端を取得
+        Vector3 bottomLeft = Camera.main.ScreenToWorldPoint(Vector3.zero);
+
+        //画面外に出る場合は、画面端の座標に変換する
+        Vector2 position = transform.position;
+        position.x = Mathf.Clamp(position.x, bottomLeft.x, bottomLeft.x * -1);
+        position.y = Mathf.Clamp(position.y, bottomLeft.y, bottomLeft.y * -1);
+        transform.position = position;
     }
 }
